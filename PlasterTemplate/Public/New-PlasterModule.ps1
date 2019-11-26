f266195099ffaeebf0d0fb68380b776c23662f19
sk82jack <sk82jack@users.noreply.github.com>
2019-11-26 23:48:52 +0000

    Update New-PlasterModule.ps1

--- PlasterTemplate/Public/New-PlasterModule.ps1
+++ PlasterTemplate/Public/New-PlasterModule.ps1
@@ -1,11 +1,3 @@
-function Get-ValidValues {
-    [CmdletBinding()]
-    param(
-        $Path
-    )
-
-    (Get-ChildItem -Path $Path -Directory).Name
-}
 Function New-PlasterModule {
     [CmdletBinding()]
     Param(
@@ -13,39 +5,54 @@ Function New-PlasterModule {
         [string]
         $ModuleName,
 
-        [Parameter(Mandatory)]
-        [ArgumentCompleter( {
-                param($Command, $Parameter, $WordToComplete, $CommandAst, $FakeBoundParams)
-
-                Get-ValidValues -Path (Resolve-Path -Path "$PSScriptRoot\..\PlasterTemplates")
-            })]
-        [ValidateScript( {$_ -in (Get-ValidValues -Path (Resolve-Path -Path "$PSScriptRoot\..\PlasterTemplates"))} )]
-        [string]
-        $TemplatePath,
-
         [Parameter()]
         [string]
-        $DestinationFolder = "$env:HOME\Documents\GitHub",
-
-        [Parameter()]
-        [string]
-        $GitHubUserName = 'sk82jack'
+        $OutPath
     )
+    DynamicParam {
+        # Set the dynamic parameters' name
+        $ParameterName = 'TemplateName'
 
+        # Create the dictionary
+        $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+
+        # Create the collection of attributes
+        $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+        # Create and set the parameters' attributes
+        $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+        $ParameterAttribute.Mandatory = $true
+        $ParameterAttribute.Position = 1
+
+        # Add the attributes to the attributes collection
+        $AttributeCollection.Add($ParameterAttribute)
+
+        # Generate and set the ValidateSet
+        $ProjectRoot = Resolve-Path "$PSScriptRoot\.."
+        $ModuleTemplatePath = Join-Path -Path $ProjectRoot -ChildPath 'PlasterTemplates'
+        $PlasterTemplates = Get-ChildItem -Path $ModuleTemplatePath -Directory
+        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($PlasterTemplates.Name)
+
+        # Add the ValidateSet to the attributes collection
+        $AttributeCollection.Add($ValidateSetAttribute)
+
+        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+        $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+        return $RuntimeParameterDictionary
+    }
+
+    Begin {
+        # Bind the parameter to a friendly variable
+        $ModuleTemplate = Join-Path -Path $ModuleTemplatePath -ChildPath $PsBoundParameters[$ParameterName]
+    }
     Process {
-        $ModulePath = Join-Path -Path $DestinationFolder -ChildPath $ModuleName
+        $ModulePath = Join-Path -Path $OutPath -ChildPath $ModuleName
         Write-Verbose "Creating module folder: $ModulePath"
-        New-Item -Path $ModulePath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-
-        $PlasterTemplatePath = Resolve-Path -Path "$PSScriptRoot\..\PlasterTemplates"
-        $ModuleTemplatePath = Join-Path -Path $PlasterTemplatePath -ChildPath $TemplatePath
-        Write-Verbose "Invoke-Plaster: Templatepath '$ModuleTemplatePath' DestinationPath '$ModulePath'"
-        Invoke-Plaster -TemplatePath $ModuleTemplatePath -DestinationPath $ModulePath
+        New-Item -Path $ModulePath -ItemType 'Directory' -Force
+        Write-Verbose "Invoke-Plaster: Templatepath '$ProjectRoot\PlasterTemplates\$TemplateName' DestinationPath '$OutPath\$ModuleName'"
+        Invoke-Plaster -TemplatePath $ModuleTemplate -DestinationPath $ModulePath
         Set-Location $ModulePath
-        git init
-        git add .
-        git commit -m 'Initial commit'
-        git remote add origin "https://github.com/$GitHubUserName/$ModuleName.git"
-        "Create an empty repo on Github called '$ModuleName' and then run the command 'git push -u origin master'"
+        $Message = "Create an empty repo on the Git server called '$ModuleName' and then run the command 'git push -u origin master'"
+        Write-Host -Object $Message -ForegroundColor Green
     }
 }
